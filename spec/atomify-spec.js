c62cc1415caf51591c61336df7b3acd3affdfc2a
jaebradley <jae.b.bradley@gmail.com>
2017-02-05 10:08:52 -0500

    add blank test

---
+++ spec/atomify-spec.js
@@ -0,0 +1,10 @@
+'use babel';
+
+import Atomify from '../lib/atomify';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('Atomify', () => { });
