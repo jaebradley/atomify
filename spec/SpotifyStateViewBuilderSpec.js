4da9a612fcfbfed85b6ee61de038cb981440e92a
jaebradley <jae.b.bradley@gmail.com>
2017-03-09 20:22:05 -0500

    add state view builder spec

---
+++ spec/SpotifyStateViewBuilderSpec.js
@@ -0,0 +1,12 @@
+'use babel';
+
+import SpotifyStateViewBuilder from '../lib/services/SpotifyStateViewBuilder';
+import SpotifyStateView from '../lib/data/SpotifyStateView';
+
+describe('Spotify State View Builder', function() {
+  it('tests image tag building', function() {
+    const testValue = 'jaebaebae';
+    const expectedImageTag = `<img class="atomify-image" src=${testValue} />`;
+    expect(SpotifyStateViewBuilder.buildImageTag(testValue)).to.eql(expectedImageTag);
+  });
+});
