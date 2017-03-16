3a78a96a5abc767fa74a29ddaa8c6fce81acb8d8
jaebradley <jae.b.bradley@gmail.com>
2017-03-16 00:08:40 -0400

    update duration translator

--- spec/duration-translator-spec.js
+++ spec/duration-translator-spec.js
@@ -7,8 +7,19 @@ describe('Duration Translator', function() {
   const translator = new DurationTranslator();
 
   it('fetch abbreviation', function() {
-    expect(translator.getAbbreviation(DurationUnit.SECOND)).toEqual('s');
+    expect(translator.getAbbreviation(DurationUnit.SECOND)).toEqual('sec');
     expect(translator.getAbbreviation(DurationUnit.MILLISECOND)).toEqual('ms');
     expect(() => translator.getAbbreviation(1)).toThrow(TypeError('Unexpected duration unit'));
+  });
+
+  it('translates duration', function() {
+    const translation = translator.translate(60, DurationUnit.SECOND);
+    const expected = {
+      days: 0,
+      hours: 0,
+      minutes: 1,
+      seconds: 0
+    };
+    expect(translation).toEqual(expected);
   });
 });
