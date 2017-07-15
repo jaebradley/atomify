36667e4679a747e42e72a5a5e18728dae9d7694d
jaebradley <jae.b.bradley@gmail.com>
2017-07-15 17:52:44 -0400

    fix test

--- spec/atomify-spec.js
+++ spec/atomify-spec.js
@@ -1,10 +1,10 @@
 'use babel';
 
-import Atomify from '../lib/atomify';
+import Main from '../lib/atom/Main';
 
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
 //
 // To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
 // or `fdescribe`). Remove the `f` to unfocus the block.
 
-describe('Atomify', () => { });
+describe('Main', () => { });
